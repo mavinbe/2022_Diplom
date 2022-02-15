6b7b90a0eb4767ce8d46c2b6f000ae8c55d3615d
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-02-15 17:20:28 +0100

    modifed 2d kalman

--- lab/2d.py
+++ lab/2d.py
@@ -32,9 +32,10 @@ class PosSensor(object):
                 self.pos[1] + randn() * self.noise_std]
 
 
-
+#Measurement Noise
 R_std = 0.35
-Q_std = 0.04
+#Process Noise
+Q_std = 0.0001
 
 
 def tracker1():
@@ -60,26 +61,30 @@ def tracker1():
 # simulate robot movement
 N = 30
 sensor = PosSensor((0, 0), (2, .2), noise_std=R_std)
+sensor_ideal = PosSensor((0, 0), (2, .2), noise_std=0)
 
+ideal = np.array([sensor_ideal.read() for _ in range(N)])
 zs = np.array([sensor.read() for _ in range(N)])
 
 # run filter
 robot_tracker = tracker1()
 mu, cov, _, _ = robot_tracker.batch_filter(zs)
 
-for x, P in zip(mu, cov):
-    # covariance of x and y
-    cov = np.array([[P[0, 0], P[2, 0]],
-                    [P[0, 2], P[2, 2]]])
-    mean = (x[0, 0], x[2, 0])
-    #plot_covariance_ellipse(mean, cov=cov, fc='g', std=3, alpha=0.5)
+# for x, P in zip(mu, cov):
+#     # covariance of x and y
+#     cov = np.array([[P[0, 0], P[2, 0]],
+#                     [P[0, 2], P[2, 2]]])
+#     mean = (x[0, 0], x[2, 0])
+#     plot_covariance_ellipse(mean, cov=cov, fc='g', std=3, alpha=0.5)
 
 # plot results
 zs *= .3048  # convert to meters
+ideal *= .3048  # convert to meters
 plot_filter(mu[:, 0], mu[:, 2])
-plot_measurements(zs[:, 0], zs[:, 1])
+plot_filter(zs[:, 0], zs[:, 1], None, 'r')
+plot_filter(ideal[:, 0], ideal[:, 1], None, 'g', "ideal")
 plt.legend(loc=2)
-plt.xlim(0, 20);
+plt.xlim(0, 20)
 
 plt.show()
 
