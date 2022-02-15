c96ab2e4ae3824eb748b245a1a99c5863c69e9d5
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-02-15 13:06:29 +0100

    first production of results of combinating detection + pose, printed to croped img files

--- movenet/movenet_pose_estimation.py
+++ movenet/movenet_pose_estimation.py
@@ -32,6 +32,7 @@ python3 examples/movenet_pose_estimation.py \
 """
 
 import argparse
+import time
 
 import numpy
 from PIL import Image
@@ -39,6 +40,8 @@ from PIL import ImageDraw
 from pycoral.adapters import common
 from pycoral.utils.edgetpu import make_interpreter
 import os
+
+from matplotlib import cm
 
 _NUM_KEYPOINTS = 17
 
@@ -86,16 +89,20 @@ KEYPOINT_COLORS = {
 file_path = os.path.dirname(__file__)
 
 
-def main():
+def main(img=None, frameId=0, detectionId=0):
+    print(numpy.shape(img))
+    img = Image.fromarray(img)
     interpreter = get_interpreter()
-    img = get_image()
+    if img is None:
+        img = get_image()
     resized_img = get_resized_img(img, interpreter)
     pose = get_pose(interpreter, resized_img)
     width, height = img.size
     pose_in_original_coordinates = transform_pose_to_orignal_coordinates(pose, width, height)
     serialized_pose = serialize_pose_list(pose_in_original_coordinates)
     write_to_result_file(serialized_pose)
-    draw_pose_and_save_img(img, pose)
+    draw_pose_and_save_img(img, pose, frameId, detectionId)
+    return pose
 
 
 def write_to_result_file(serialized_pose):
@@ -125,7 +132,7 @@ def transform_pose_to_orignal_coordinates(pose, width, height):
     return pose_clone
 
 
-def draw_pose_and_save_img(img, pose):
+def draw_pose_and_save_img(img, pose, frameId, detectionId):
     draw = ImageDraw.Draw(img)
     width, height = img.size
     for i in range(0, _NUM_KEYPOINTS):
@@ -136,13 +143,21 @@ def draw_pose_and_save_img(img, pose):
                     pose[i][1] * width + 2, pose[i][0] * height + 2
                 ],
                 fill=KEYPOINT_COLORS[i])
-    img.save(file_path + "/result.bmp")
+    img.save(file_path + f"/result_{frameId}_{detectionId}.bmp")
 
 
 def get_pose(interpreter, resized_img):
     common.set_input(interpreter, resized_img)
-    interpreter.invoke()
-    pose = common.output_tensor(interpreter, 0).copy().reshape(_NUM_KEYPOINTS, 3)
+    print('----INFERENCE TIME----')
+    print('Note: The first inference is slow because it includes',
+          'loading the model into Edge TPU memory.')
+    for _ in range(5):
+        start = time.perf_counter()
+        interpreter.invoke()
+        inference_time = time.perf_counter() - start
+        pose = common.output_tensor(interpreter, 0).copy().reshape(_NUM_KEYPOINTS, 3)
+        print('%.2f ms' % (inference_time * 1000))
+
     return pose
 
 
@@ -159,8 +174,10 @@ def get_interpreter():
 
 def get_image():
     img = Image.open(file_path + "/squat.bmp")
+    print(type(img))
     return img
 
 
 if __name__ == '__main__':
+    print('MAIN')
     main()
