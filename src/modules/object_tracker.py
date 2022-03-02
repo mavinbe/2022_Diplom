c91d774e53e481938b3c7cc909f098d719f7f84d
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-03-02 14:04:07 +0100

    object_tracker.py: use yolo5_dataset

--- src/modules/object_tracker.py
+++ src/modules/object_tracker.py
@@ -22,7 +22,7 @@ import torch.backends.cudnn as cudnn
 from yolov5.models.experimental import attempt_load
 from yolov5.utils.downloads import attempt_download
 from yolov5.models.common import DetectMultiBackend
-from yolov5.utils.datasets import LoadImages, LoadStreams
+from dataset.yolo5_dataset import LoadImages
 from yolov5.utils.general import (LOGGER, check_img_size, non_max_suppression, scale_coords,
                                   check_imshow, xyxy2xywh, increment_path)
 from yolov5.utils.torch_utils import select_device, time_sync
