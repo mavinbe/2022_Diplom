1538bd383573ad40c03d775181dc20514659b3ab
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-03-07 09:56:41 +0100

    Yolo5: update requirements.txt, so versions are fixed, added _lambda=0 and add more verbose benchmark for deepsort

--- src/modules/object_tracker.py
+++ src/modules/object_tracker.py
@@ -56,6 +56,7 @@ class ObjectTracker:
                                 max_dist=cfg.DEEPSORT.MAX_DIST,
                                 max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
                                 max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
+                                _lambda=0
                                 )
 
             # Initialize
