8e40dee0629eec50ceeb8936a6c1ce84adde2989
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-03-07 13:20:13 +0100

    run_pipeline.py: follow newest track in pixel style

--- src/app/run_pipeline.py
+++ src/app/run_pipeline.py
@@ -9,10 +9,34 @@ from yolov5.utils.torch_utils import time_sync
 
 PoseLandmark = mp.solutions.pose.PoseLandmark
 
-img_stream = cv2.VideoCapture("/home/mavinbe/2021_Diplom/2022_Diplom/data/05_20211102141647/output015.mp4")
-with PoseDetector() as pose_detector:
+img_stream = cv2.VideoCapture("/home/mavinbe/2021_Diplom/2022_Diplom/data/05_20211102141647/output014.mp4")
 
-    object_tracker = ObjectTracker()
+
+def calculate_newest_track_id():
+    return max(object_detection_dict.keys())
+
+
+def translate_local_to_global_coords(pose_dict, global_x, global_y):
+    pose_dict = pose_dict.copy()
+    for key in pose_dict:
+        pose_dict[key]['x'] = int(pose_dict[key]['x'] + global_x)
+        pose_dict[key]['y'] = int(pose_dict[key]['y'] + global_y)
+
+    return pose_dict
+
+def zoom(img, zoom_factor, center):
+    height, width, _ = image.shape
+    x_top = int(center[0] - width / zoom_factor / 2)
+    x_bottom = int(center[0] + width / zoom_factor / 2)
+    y_left = int(center[1] - height / zoom_factor / 2)
+    y_right = int(center[1] + height / zoom_factor / 2)
+    img = img[y_left:y_right, x_top:x_bottom]
+    return cv2.resize(img, None, fx=zoom_factor, fy=zoom_factor, interpolation=cv2.INTER_NEAREST)
+
+
+with PoseDetector(show_vid=True) as pose_detector:
+
+    object_tracker = ObjectTracker(show_vid=False)
 
     while img_stream.isOpened():
         t1 = time_sync()
@@ -24,21 +48,37 @@ with PoseDetector() as pose_detector:
             print("Ignoring empty camera frame.")
             # If loading a video, use 'break' instead of 'continue'.
             continue
-        detection_dict = object_tracker.inference_frame(image)
+        object_detection_dict = object_tracker.inference_frame(image)
         t3 = time_sync()
 
-        for track_id, detection in detection_dict.items():
+        if len(object_detection_dict) > 0:
+            newest_track_id = calculate_newest_track_id()
+            detection_which_to_pose_detect = object_detection_dict[newest_track_id]
+            cropped_image = image[detection_which_to_pose_detect[1]:detection_which_to_pose_detect[3], detection_which_to_pose_detect[0]:detection_which_to_pose_detect[2]]
+            pose_detect_dict = pose_detector.inference_frame(cropped_image)
+            pose_detect_dict = translate_local_to_global_coords(pose_detect_dict, detection_which_to_pose_detect[0], detection_which_to_pose_detect[1])
+            if PoseLandmark.NOSE in pose_detect_dict and PoseLandmark.LEFT_EYE in pose_detect_dict and PoseLandmark.RIGHT_EYE in pose_detect_dict:
+                print("LEFT " + str(pose_detect_dict[PoseLandmark.NOSE]['z'] - pose_detect_dict[PoseLandmark.LEFT_EYE]['z']) + "\t\t" + str(pose_detect_dict[PoseLandmark.NOSE]['z'] - pose_detect_dict[PoseLandmark.RIGHT_EYE]['z']))
+                # cv2.circle(image, (pose_detect_dict[PoseLandmark.NOSE]['x'], pose_detect_dict[PoseLandmark.NOSE]['y']),
+                #            5,
+                #            (0, 0, 255), 2)
+                # cv2.circle(image,
+                #            (pose_detect_dict[PoseLandmark.LEFT_EYE]['x'], pose_detect_dict[PoseLandmark.LEFT_EYE]['y']),
+                #            4,
+                #            (0, 255, 0), 2)
+                # cv2.circle(image,
+                #            (pose_detect_dict[PoseLandmark.RIGHT_EYE]['x'], pose_detect_dict[PoseLandmark.RIGHT_EYE]['y']),
+                #            4,
+                #            (0, 255, 0), 2)
+                image = zoom(image,40 , (pose_detect_dict[PoseLandmark.NOSE]['x'],pose_detect_dict[PoseLandmark.NOSE]['y']))
 
-            pose_dict = pose_detector.inference_frame(image[detection[1]:detection[3], detection[0]:detection[2]])
+            else:
+                print("No Nose")
 
-            for key in pose_dict:
-                pose_dict[key]['x'] = int(pose_dict[key]['x'] + detection[0])
-                pose_dict[key]['y'] = int(pose_dict[key]['y'] + detection[1])
 
-            #print(pose_dict[PoseLandmark.NOSE])
-            break
+            cv2.imshow("asd", image)
         t4 = time_sync()
 
-        LOGGER.info(f'DONE on hole :({(t4 - t1)*1000:.3f}ms)    read_image:({(t2 - t1)*1000:.3f}ms), object_track:({(t3 - t2)*1000:.3f}ms), pose_detect:({(t4 - t3)*1000:.3f}ms)')
+        #LOGGER.info(f'DONE on hole :({(t4 - t1)*1000:.3f}ms)    read_image:({(t2 - t1)*1000:.3f}ms), object_track:({(t3 - t2)*1000:.3f}ms), pose_detect:({(t4 - t3)*1000:.3f}ms)')
 
 img_stream.release()
