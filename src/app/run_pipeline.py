a6be06f0a0f1123d3e5cbf6d69397bb99ba01c3d
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-03-08 14:22:14 +0100

    run_pipeline.py: refactoring, so we can determ target_zoom_box for camera controll model

--- src/app/run_pipeline.py
+++ src/app/run_pipeline.py
@@ -15,6 +15,7 @@ import imageio
 def calculate_newest_track_id():
     return max(object_detection_dict.keys())
 
+
 def calculate_oldest_track_id():
     return min(object_detection_dict.keys())
 
@@ -27,23 +28,37 @@ def translate_local_to_global_coords(pose_dict, global_x, global_y):
 
     return pose_dict
 
-def zoom(img, zoom_factor, center=None):
-    height, width, _ = image.shape
+
+def zoom(img, target_box):
+    height, width, _ = img.shape
+    img = img[target_box[2]:target_box[3], target_box[0]:target_box[1]]
+    try:
+        img = cv2.resize(img, (width, height), interpolation=cv2.INTER_NEAREST)
+    except:
+        print(target_box)
+        print(img.shape)
+
+    return img
+
+
+def determ_target_box(image_shape, pose_detect_dict):
+    if PoseLandmark.NOSE in pose_detect_dict:
+        return static_zoom_target_box(image_shape, 20, (
+            pose_detect_dict[PoseLandmark.NOSE]['x'], pose_detect_dict[PoseLandmark.NOSE]['y']))
+    else:
+        print("No Nose")
+
+
+def static_zoom_target_box(image_shape, zoom_factor, center):
+    height, width, _ = image_shape
     if center is None:
-        center = (width / 2 , height / 2)
+        center = (width / 2, height / 2)
     x_left = int(center[0] - width / zoom_factor / 2)
     x_right = int(center[0] + width / zoom_factor / 2)
     y_top = int(center[1] - height / zoom_factor / 2)
     y_bottom = int(center[1] + height / zoom_factor / 2)
-    img = img[y_top:y_bottom, x_left:x_right]
-    try:
+    return x_left, x_right, y_top, y_bottom
 
-        img = cv2.resize(img, None, fx=zoom_factor, fy=zoom_factor, interpolation=cv2.INTER_NEAREST)
-    except:
-        print(f'[{x_left}:{x_right},{y_top}:{y_bottom}]')
-        print(img.shape)
-
-    return  img
 
 def calculate_face_direction(pose_detect_dict):
     ## Sample code
@@ -81,40 +96,23 @@ def run(handle_image):
             t3 = time_sync()
 
             if len(object_detection_dict) > 0:
-                track_id_to_track = calculate_oldest_track_id()
+                track_id_to_track = calculate_newest_track_id()
                 detection_which_to_pose_detect = object_detection_dict[track_id_to_track]
-                print(detection_which_to_pose_detect)
+                # print(detection_which_to_pose_detect)
                 cropped_image = image[detection_which_to_pose_detect[1]:detection_which_to_pose_detect[3],
                                 detection_which_to_pose_detect[0]:detection_which_to_pose_detect[2]]
                 pose_detect_dict = pose_detector.inference_frame(cropped_image)
                 pose_detect_dict = translate_local_to_global_coords(pose_detect_dict, detection_which_to_pose_detect[0],
                                                                     detection_which_to_pose_detect[1])
-                if PoseLandmark.NOSE in pose_detect_dict and PoseLandmark.LEFT_EYE in pose_detect_dict and PoseLandmark.RIGHT_EYE in pose_detect_dict:
-                    print("LEFT " + str(
-                        pose_detect_dict[PoseLandmark.NOSE]['z'] - pose_detect_dict[PoseLandmark.LEFT_EYE][
-                            'z']) + "\t\t" + str(
-                        pose_detect_dict[PoseLandmark.NOSE]['z'] - pose_detect_dict[PoseLandmark.RIGHT_EYE]['z']))
-                    # cv2.circle(image, (pose_detect_dict[PoseLandmark.NOSE]['x'], pose_detect_dict[PoseLandmark.NOSE]['y']),
-                    #            5,
-                    #            (0, 0, 255), 2)
-                    # cv2.circle(image,
-                    #            (pose_detect_dict[PoseLandmark.LEFT_EYE]['x'], pose_detect_dict[PoseLandmark.LEFT_EYE]['y']),
-                    #            4,
-                    #            (0, 255, 0), 2)
-                    # cv2.circle(image,
-                    #            (pose_detect_dict[PoseLandmark.RIGHT_EYE]['x'], pose_detect_dict[PoseLandmark.RIGHT_EYE]['y']),
-                    #            4,
-                    #            (0, 255, 0), 2)
-                    image = zoom(image, 20,
-                                 (pose_detect_dict[PoseLandmark.NOSE]['x'], pose_detect_dict[PoseLandmark.NOSE]['y']))
 
-                else:
-                    print("No Nose")
+                target_box = determ_target_box(image.shape, pose_detect_dict)
+                image = zoom(image, target_box)
 
                 handle_image(image)
             t4 = time_sync()
 
-            # LOGGER.info(f'DONE on hole :({(t4 - t1)*1000:.3f}ms)    read_image:({(t2 - t1)*1000:.3f}ms), object_track:({(t3 - t2)*1000:.3f}ms), pose_detect:({(t4 - t3)*1000:.3f}ms)')
+            LOGGER.info(
+                f'DONE on hole :({(t4 - t1) * 1000:.3f}ms)    read_image:({(t2 - t1) * 1000:.3f}ms), object_track:({(t3 - t2) * 1000:.3f}ms), pose_detect:({(t4 - t3) * 1000:.3f}ms)')
     img_stream.release()
 
 
@@ -125,5 +123,4 @@ def show_image(image):
 
 
 if __name__ == '__main__':
-
     run(show_image)
