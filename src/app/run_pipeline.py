426eeb54b85f67ccc04603185f5c879ee776ca91
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-03-11 11:15:03 +0100

    run_pipeline.py: refactoring

--- src/app/run_pipeline.py
+++ src/app/run_pipeline.py
@@ -1,20 +1,14 @@
-from os import wait
-
 import cv2
 import mediapipe as mp
 import numpy as np
-import torch
 
 from modules.object_tracker import ObjectTracker
 from modules.pose_detector import PoseDetector
 from utils.general import LOGGER
-
 from modules.pysical_models.new_position_max_speed_constrained import NewPositionMaxSpeedConstrained
 from yolov5.utils.torch_utils import time_sync
 
 PoseLandmark = mp.solutions.pose.PoseLandmark
-
-
 
 
 def calculate_newest_track_id(object_detection_dict):
@@ -48,8 +42,8 @@ def zoom(img, target_box):
 
 
 def determ_position_by_landmark_from_pose_detection(pose_detect_dict, landmark):
-    if PoseLandmark.NOSE in pose_detect_dict:
-        return pose_detect_dict[PoseLandmark.NOSE]['x'], pose_detect_dict[PoseLandmark.NOSE]['y']
+    if landmark in pose_detect_dict:
+        return pose_detect_dict[landmark]['x'], pose_detect_dict[landmark]['y']
 
 
 def static_zoom_target_box(image_shape, zoom_factor, center):
@@ -77,65 +71,88 @@ def static_zoom_target_box(image_shape, zoom_factor, center):
     return y_top, y_bottom, x_left, x_right
 
 
-
 def run(handle_image):
-    
-    img_stream = cv2.VideoCapture("/home/mavinbe/2021_Diplom/2022_Diplom/data/05_20211102141647/output014.mp4")
-    #img_stream.set(cv2.CAP_PROP_POS_FRAMES, 290)
     with PoseDetector(show_vid=False) as pose_detector:
 
         object_tracker = ObjectTracker(show_vid=False)
         frame_count = 0
-        position_model = None
-        success, image = img_stream.read()
-        img_stream.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        height, width, _ = image.shape
+        img_stream = cv2.VideoCapture("/home/mavinbe/2021_Diplom/2022_Diplom/data/05_20211102141647/output014.mp4")
+        height, width = determ_dimensions_of_video(img_stream)
         position_model = NewPositionMaxSpeedConstrained(time_sync(), np.asarray((int(width / 2), int(height / 2))), 20)
-        while img_stream.isOpened():
-            if frame_count > 1600:
-                exit()
-            t1 = time_sync()
-            success, image = img_stream.read()
-            t2 = time_sync()
 
+        while img_stream.isOpened():
+
+            t_start = time_sync()
+
+            # t_read_image
+            image, success = read_frame_till_x(img_stream, frame_count, 1600)
+            frame_count += 1
             if not success:
-                print("Ignoring empty camera frame.")
-                # If loading a video, use 'break' instead of 'continue'.
+                continue
+            t_read_image = time_sync()
+
+            # t_object_track
+            object_detection_dict = object_tracker.inference_frame(image)
+            if len(object_detection_dict) == 0:
+                print("No Objects Detected")
+                continue
+            t_object_track = time_sync()
+
+            # t_pose_detect
+            pose_detect_dict, pose_detect_dict_in_global = inference_pose(pose_detector, image, object_detection_dict,
+                                                                          calculate_newest_track_id)
+            t_pose_detect = time_sync()
+
+            # t_post
+            target_position = determ_position_by_landmark_from_pose_detection(pose_detect_dict_in_global,
+                                                                              PoseLandmark.NOSE)
+            if target_position is None:
+                print("No Landmark found " + str(PoseLandmark.NOSE))
                 continue
 
-            frame_count += 1
-            object_detection_dict = object_tracker.inference_frame(image)
-            t3 = time_sync()
+            position_model.move_to_target(target_position, time_sync())
+            target_box = static_zoom_target_box(image.shape, 20, position_model.get_position())
+            image = zoom(image, target_box)
+            t_post = time_sync()
 
+            # t_handle_image
+            handle_image(image)
 
-
-            if len(object_detection_dict) > 0:
-                track_id_to_track = calculate_newest_track_id(object_detection_dict)
-                detection_which_to_pose_detect = object_detection_dict[track_id_to_track]
-
-                cropped_image = image[detection_which_to_pose_detect[1]:detection_which_to_pose_detect[3],
-                                detection_which_to_pose_detect[0]:detection_which_to_pose_detect[2]]
-                pose_detect_dict = pose_detector.inference_frame(cropped_image)
-                pose_detect_dict_in_global = translate_local_to_global_coords(pose_detect_dict, detection_which_to_pose_detect[0],
-                                                                    detection_which_to_pose_detect[1])
-
-                target_position = determ_position_by_landmark_from_pose_detection(pose_detect_dict_in_global, PoseLandmark.NOSE)
-
-                if target_position:
-                    position_model.move_to_target(target_position, time_sync())
-
-                else:
-                    print("No Landmark")
-                target_box = static_zoom_target_box(image.shape, 20, position_model.get_position())
-
-                image = zoom(image, target_box)
-
-                handle_image(image)
-            t4 = time_sync()
+            t_handle_image = time_sync()
 
             LOGGER.info(
-                f'frame_count {frame_count} DONE on hole :({(t4 - t1) * 1000:.3f}ms)    read_image:({(t2 - t1) * 1000:.3f}ms), object_track:({(t3 - t2) * 1000:.3f}ms), pose_detect:({(t4 - t3) * 1000:.3f}ms)')
+                f'frame_count {frame_count} DONE on hole :({(t_handle_image - t_start) * 1000:.3f}ms)    read_image:({(t_read_image - t_start) * 1000:.3f}ms), object_track:({(t_object_track - t_read_image) * 1000:.3f}ms), pose_detect:({(t_pose_detect - t_object_track) * 1000:.3f}ms), post:({(t_post - t_pose_detect) * 1000:.3f}ms), handle_image:({(t_handle_image - t_post) * 1000:.3f}ms)')
     img_stream.release()
+
+
+def determ_dimensions_of_video(img_stream):
+    current_frame_id = img_stream.get(cv2.CAP_PROP_POS_FRAMES)
+    _, init_frame = img_stream.read()
+    img_stream.set(cv2.CAP_PROP_POS_FRAMES, current_frame_id)
+    height, width, _ = init_frame.shape
+    return height, width
+
+
+def inference_pose(pose_detector, image, object_detection_dict, calculate_track_id_strategy):
+    track_id_to_track = calculate_track_id_strategy(object_detection_dict)
+    detection_which_to_pose_detect = object_detection_dict[track_id_to_track]
+
+    cropped_image = image[detection_which_to_pose_detect[1]:detection_which_to_pose_detect[3],
+                    detection_which_to_pose_detect[0]:detection_which_to_pose_detect[2]]
+    pose_detect_dict = pose_detector.inference_frame(cropped_image)
+    pose_detect_dict_in_global = translate_local_to_global_coords(pose_detect_dict,
+                                                                  detection_which_to_pose_detect[0],
+                                                                  detection_which_to_pose_detect[1])
+    return pose_detect_dict, pose_detect_dict_in_global
+
+
+def read_frame_till_x(img_stream, frame_count, x):
+    if frame_count >= x:
+        exit()
+    success, image = img_stream.read()
+    if not success:
+        print("Ignoring empty camera frame.")
+    return image, success
 
 
 def show_image(image):
