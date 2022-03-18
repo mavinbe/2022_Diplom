965ae9e0e4fcc345289234fcce3df4a6d6016e72
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-03-18 10:11:30 +0100

    refactor

--- test/modules/pysical_models/test_new_position_max_acceleration_based.py
+++ test/modules/pysical_models/test_new_position_max_acceleration_based.py
@@ -51,65 +51,57 @@ def plot_movement(sut, a_0, s_0, v_0, v_target, t_where_v_is_target, s_target, t
 
 class TestCalculateSpeed(unittest.TestCase):
 
-    def test_straighte_punktlandung(self):
-        a_0 = -1
-        v_0 = 3
-        s_0 = 0
-
-        sut = AccelerationMovementModel
-
-        v_target = 0
-        s_target = 4.5
-
-        t_where_v_is_0 = sut.calculate_t_given_v(v=v_target, a=a_0, v_0=v_0)
-        #t_where_s_is_target = sut.calculate_t_given_s(v=v_target, a=a_0, v_0=v_0)
-        #plot_movement(sut, a_0, s_0, v_0, v_target, s_target)
-        self.assertEqual(t_where_v_is_0, 3)
-        self.assertEqual(sut.s(t=t_where_v_is_0, a=a_0, v_0=v_0, s_0=s_0), 4.5)
-
     @parameterized.expand([
-        [{"a_0": -1,
-          "v_0": 3,
-          "s_0": 0,
-          "v_target": 0,
-          "s_target": 4.5,
-          "t_where_v_is_target": 3,
-          "s_on_t_where_v_is_target": 4.5,
-          "t_next_where_s_is_target": 3,
-          "title": "perfekt match"
-          }],
-        [{"a_0": -1,
-          "v_0": 3,
-          "s_0": 0,
-          "v_target": 0,
-          "s_target": 4,
-          "t_where_v_is_target": 3,
-          "s_on_t_where_v_is_target": 4.5,
-          "t_next_where_s_is_target": 2,
-          "title": "to fast"
-          }],
-        [{"a_0": -1,
-          "v_0": 2,
-          "s_0": 0,
-          "v_target": 0,
-          "s_target": 4,
-          "t_where_v_is_target": 2,
-          "s_on_t_where_v_is_target": 2,
-          "t_next_where_s_is_target": None,
-          "title": "to slow"
-          }],
-        [{"a_0": -1,
-          "v_0": 4,
-          "s_0": 0,
-          "v_target": 0,
-          "s_target": 4,
-          "t_where_v_is_target": 4,
-          "s_on_t_where_v_is_target": 8,
-          "t_next_where_s_is_target": 1.1715728752538097,
-          "title": "to fast 2"
+        [{
+            "title": "perfekt match",
+            "a_0": -1,
+            "v_0": 3,
+            "s_0": 0,
+            "v_target": 0,
+            "s_target": 4.5,
+            "t_where_v_is_target": 3,
+            "s_on_t_where_v_is_target": 4.5,
+            "t_next_where_s_is_target": 3,
+            "a_is" : "perfekt"
+        }],
+        [{
+            "title": "to fast",
+            "a_0": -1,
+            "v_0": 3,
+            "s_0": 0,
+            "v_target": 0,
+            "s_target": 4,
+            "t_where_v_is_target": 3,
+            "s_on_t_where_v_is_target": 4.5,
+            "t_next_where_s_is_target": 2,
+            "a_is" : "to high"
+        }],
+        [{
+            "title": "to slow",
+            "a_0": -1,
+            "v_0": 2,
+            "s_0": 0,
+            "v_target": 0,
+            "s_target": 4,
+            "t_where_v_is_target": 2,
+            "s_on_t_where_v_is_target": 2,
+            "t_next_where_s_is_target": None,
+            "a_is" : "to low"
+        }],
+        [{
+            "title": "to fast 2",
+            "a_0": -1,
+            "v_0": 4,
+            "s_0": 0,
+            "v_target": 0,
+            "s_target": 4,
+            "t_where_v_is_target": 4,
+            "s_on_t_where_v_is_target": 8,
+            "t_next_where_s_is_target": 1.1715728752538097,
+            "a_is" : "to high"
           }],
     ])
-    def test_static_t_from_s(self, sdf):
+    def test_t_from_s(self, sdf):
         a_0 = sdf["a_0"]
         v_0 = sdf["v_0"]
         s_0 = sdf["s_0"]
@@ -151,7 +143,7 @@ class TestAccelerationMovementModel(unittest.TestCase):
         self.assertEqual(sut.v(t=1, a=2, v_0=0), 2)
         self.assertEqual(sut.v(t=2, a=2, v_0=0), 4)
 
-    def test_t_from_v(self):
+    def test_calculate_t_given_v(self):
         sut = AccelerationMovementModel
 
         self.assertEqual(sut.calculate_t_given_v(v=0, a=-1, v_0=3), 3)
@@ -159,7 +151,6 @@ class TestAccelerationMovementModel(unittest.TestCase):
 
         self.assertEqual(sut.calculate_t_given_v(v=1, a=-1, v_0=3), 2)
         self.assertEqual(sut.s(t=2, a=-1, v_0=3, s_0=0), 4)
-
 
 
     def test_s(self):
