fc3fffb29ba217db7d8b9a66551980b497916db4
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-03-18 15:56:07 +0100

    refactor

--- src/modules/pysical_models/acceleration_movement_model.py
+++ src/modules/pysical_models/acceleration_movement_model.py
@@ -5,20 +5,24 @@ import numpy as np
 
 class AccelerationMovementModel:
 
-    @staticmethod
-    def v(t, a, v_0):
+    @classmethod
+    def state(cls, t, a, v_0, s_0):
+        return a, cls.v(t, a, v_0), cls.s(t, a, v_0, s_0)
+
+    @classmethod
+    def v(cls, t, a, v_0):
         return a * t + v_0
 
-    @staticmethod
-    def s(t, a, v_0, s_0):
+    @classmethod
+    def s(cls, t, a, v_0, s_0):
         return a / 2 * t ** 2 + v_0 * t + s_0
 
-    @staticmethod
-    def calculate_t_given_v(v, a, v_0):
+    @classmethod
+    def calculate_t_given_v(cls, v, a, v_0):
         return (v - v_0) / a
 
-    @staticmethod
-    def calculate_t_given_s(s, a, v_0, s_0):
+    @classmethod
+    def calculate_t_given_s(cls, s, a, v_0, s_0):
         front = - (v_0/a)
         in_sqrt =  (v_0/a)**2 - 2*s_0/a + 2*s/a
         back = None
@@ -30,9 +34,9 @@ class AccelerationMovementModel:
             print(in_sqrt)
         return (front - back, front + back)
 
-    @staticmethod
-    def calculate_t_given_s_nearest_ahead(s, a, v_0, s_0):
-        t = AccelerationMovementModel.calculate_t_given_s(s, a, v_0, s_0)
+    @classmethod
+    def calculate_t_given_s_nearest_ahead(cls, s, a, v_0, s_0):
+        t = cls.calculate_t_given_s(s, a, v_0, s_0)
         if t is None:
             return None
         if 0 > t[0] and 0 > t[1]:
