f7a6cacd01f15f2fb6cdb2c31da23ff6e62ada2e
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-03-24 11:45:15 +0100

    cam_to_screen: destroy stream correct

--- lab/cam_to_screen.py
+++ lab/cam_to_screen.py
@@ -1,12 +1,17 @@
 import cv2
 import screeninfo
 
-cap = cv2.VideoCapture("rtsp://malte:diplom@192.168.0.105:554//h264Preview_06_main")
+import atexit
+
+#from media.VideoStreamProvider import VideoStreamProvider
+
+cap = cv2.VideoCapture("rtsp://malte:diplom@192.168.0.105:554//h264Preview_07_main")
+atexit.register(cap.release)
 screen = screeninfo.get_monitors()[0]
-print(f"screen {screen}")
-cv2.namedWindow("frame", cv2.WND_PROP_FULLSCREEN)
-cv2.moveWindow("frame", int(screen.x - 1), int(screen.y - 1))
-cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+# print(f"screen {screen}")
+# cv2.namedWindow("frame", cv2.WND_PROP_FULLSCREEN)
+# cv2.moveWindow("frame", int(screen.x - 1), int(screen.y - 1))
+# cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 
 while(True):
@@ -15,11 +20,13 @@ while(True):
 
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = frame[:-180, 60:-60]
+    #frame = cv2.resize(frame, (screen.width, screen.height))
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
-    print(f"frame.shape {frame.shape}")
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # print(f"frame.shape {frame.shape}")
+    if cv2.waitKey(100) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
