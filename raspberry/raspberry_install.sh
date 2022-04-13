001e06070791e9ef53a7c169fe889e2b4c8f29a1
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-04-13 10:56:09 +0200

    opencv-python compile on ubuntu

--- raspberry/raspberry_install.sh
+++ raspberry/raspberry_install.sh
@@ -134,3 +134,24 @@ sudo apt-get update
 
 cd ~/.virtualenvs/cv440/lib/python3.9/site-packages
 ln -s /usr/local/lib/python3.9/site-packages/cv2/python-3.9/cv2.cpython-39-arm-linux-gnueabihf.so
+
+
+
+#### compile opencv-python with gstreamer support on ubuntu
+
+
+# https://discuss.bluerobotics.com/t/opencv-python-with-gstreamer-backend/8842
+
+sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+
+# <navigate to where you want the opencv-python repo to be stored>
+git clone --recursive https://github.com/skvark/opencv-python.git
+cd opencv-python
+export CMAKE_ARGS="-DWITH_GSTREAMER=ON"
+pip install --upgrade pip wheel
+# this is the build step - the repo estimates it can take from 5
+#   mins to > 2 hrs depending on your computer hardware
+pip wheel . --verbose
+pip install opencv_python*.whl
+# note, wheel may be generated in dist/ directory, so may have to cd first
+
