872f683c95799a18bf656f2faa05cc785fb10f20
Malte-Levin Behrens <malte.levin.behrens@gmail.com>
2022-04-13 13:28:53 +0200

    lab: running send and receive opencv gstreamer python example

--- lab/stream_to_screen.py
+++ lab/stream_to_screen.py
@@ -1,3 +1,6 @@
+# receive from cam_to_stream.py
+
+
 #import cv2
 
 # gst-launch-1.0 -v playbin uri=rtsp://malte:diplom@192.168.0.105:554//h264Preview_07_main uridecodebin0::source::latency=300
@@ -18,8 +21,7 @@
 #
 # receive command
 # gst-launch-1.0 udpsrc port=5000 ! application/x-rtp, media=video, encoding-name=JPEG, framerate=30/1, payload=26, clock-rate=90000 ! rtpjpegdepay ! jpegdec ! videoconvert ! autovideosink
-
-
+import time
 
 import cv2
 import screeninfo
@@ -27,22 +29,31 @@ import screeninfo
 import atexit
 print("asd")
 
-cap = cv2.VideoCapture('udpsrc port=5000 ! application/x-rtp, media=video, encoding-name=JPEG, framerate=30/1, payload=26, clock-rate=90000 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+cap = cv2.VideoCapture('udpsrc port=7001 !  application/x-rtp-stream,encoding-name=JPEG ! rtpstreamdepay ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
 
 atexit.register(cap.release)
 screen = screeninfo.get_monitors()[0]
 
-
+print(f"screen {screen}")
+#cv2.namedWindow("frame", cv2.WND_PROP_FULLSCREEN)
+#cv2.moveWindow("frame", int(screen.x - 1), int(screen.y - 1))
+#cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    #print((ret, frame))
+    if frame is None:
+        time.sleep(1)
+        continue
 
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #frame = frame[:-180, 60:-60]
-    #frame = cv2.resize(frame, (860, 620))
-    print(frame.shape)
+    #print(frame)
+    #print((screen.width, screen.height))
+    #frame = cv2.resize(frame, (screen.width, screen.height))
+    #print(frame.shape)
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
